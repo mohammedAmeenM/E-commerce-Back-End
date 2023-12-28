b1af8cb16359f75e211b094786e8e8890f1b2f3f
amiiiiee <amiameen507@gmail.com>
2023-12-28 09:50:04 +0530

    editt

--- src/app.js
+++ src/app.js
@@ -2,10 +2,15 @@ const express=require('express');
 const userRouter = require('./apis/Router/userRouter');
 const app=express();
 const mogan=require('morgan')
+const customError=require('./apis/utils/customError')
 
 
 app.use(express.json());
 app.use(mogan('dev'))
 app.use('/api/users/',userRouter)
+app.all('*',(req,res,next)=>{
+    const err=new customError (`can't find ${req.originalUrl} on the server ! `,404)
+    next(err)
+})
 
 module.exports=app;
