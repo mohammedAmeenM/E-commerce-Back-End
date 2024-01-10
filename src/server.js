ce7a50801d00663a1d466c27452f014dd3506a34
amiiiiee <amiameen507@gmail.com>
2024-01-10 09:39:57 +0530

    edit-order

--- src/server.js
+++ src/server.js
@@ -1,13 +1,10 @@
-const dotenv=require('dotenv');
-dotenv.config({path:'./config/config.env'});
-const app=require('./app');
-const connect=require('./config/db_conection')
+const dotenv = require("dotenv");
+dotenv.config({ path: "./config/config.env" });
+const app = require("./app");
+const connect = require("./config/db_conection");
 
-
-connect()
-const port =process.env.PORT;
-app.listen(port,()=>{
-    console.log(`server running is :${port}`)
-})
-
-
+connect();
+const port = process.env.PORT;
+app.listen(port, () => {
+  console.log(`server running is :${port}`);
+});
